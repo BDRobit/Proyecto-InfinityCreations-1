5fcb087a27c17b710c590b04273c6919e40e28d3
hector <blackdragon.robit0@gmail.com>
2023-06-15 00:09:17 -0400

    agregando personaje.py a modo prueba de pc casa

---
+++ personaje.py
@@ -0,0 +1,8 @@
+
+
+class Personaje():
+    def __init__(self, id_personaje, nombreReal):
+        self.__id_personaje = id_personaje
+        self.__nombreReal = nombreReal
+
+"""        ejecutando prueba  """
